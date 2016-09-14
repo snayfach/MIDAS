7f64a5d7cd48a1179a416a11d6f1b86c032304a3
snayfach <snayfach@gmail.com>
2016-09-13 21:33:49 -0700

    Updated diversity code

--- midas/analyze/snp_matrix.py
+++ midas/analyze/snp_matrix.py
@@ -20,50 +20,86 @@ class GenomicSite:
 			self.samples = samples
 			self.cast_numeric()
 			self.hq_samples = len([_ for _ in samples if _.pass_qc])
-			
 		except StopIteration:
 			self.id = None
 
-	def filter_site(self, site_prev, site_maf, site_types):
+	def cast_numeric(self):
+		""" format numeric fields """
+		self.ref_freq = [float(_) if _ != 'NA' else 0.0 for _ in self.ref_freq ]
+		self.depth = [int(_) if _ != 'NA' else 0.0 for _ in self.depth]
+	
+	def parse_id(self):
+		""" parse info from site_id """
+		ref_id = self.id.rsplit('|', 2)[0]
+		ref_pos = int(self.id.split('|')[2])
+		ref_allele = self.id.split('|')[3] if len(self.id.split('|')) >= 4 else '' # testing
+		return ref_id, ref_pos, ref_allele
+		
+	def filter_site(self, site_prev, site_maf, site_type, site_freq):
 		""" determine if site passes quality control """
-		if self.prev < max(1e-6, site_prev): # must be data for at least 1 sample
-			return True
+		if self.prev < max(1e-6, site_prev):
+			return True, 'site_prev'
 		elif self.maf < site_maf:
-			return True
+			return True, 'site_maf'
 		elif self.ref_allele not in ['A','T','C','G']:
-			return True
-		elif self.info['site_type'] not in site_types:
-			return True
+			return True, 'ref_allele'
+		elif self.info['site_type'] not in site_type:
+			return True, 'site_type'
+		elif self.refalt_freq < site_freq:
+			return True, 'site_freq'
 		else:
-			return False
+			return False, None
 
 	def summary_stats(self, weighted):
+		""" compute summary stats for site across hq samples """
 		self.prev = float(len(self.samples))/self.hq_samples
-		mean_freq = self.mean_freq(weighted)
-		self.maf = min(mean_freq, 1-mean_freq)
+		self.mean_freq = self.compute_mean_freq(weighted)
+		self.maf = min(self.mean_freq, 1-self.mean_freq)
+		self.allele_freqs = self.compute_allele_freqs()
+		self.refalt_freq = self.compute_refalt_freq()
+		
+	def compute_allele_freqs(self):
+		""" compute combined frequency of 4 alleles across samples; only considers 2 alleles per sample """
+		freqs = {'A':0.0, 'T':0.0, 'C':0.0, 'G':0.0}
+		for freq, alt_allele in zip(self.ref_freq, self.alt_allele):
+			freqs[self.ref_allele] += float(freq)
+			if alt_allele != 'NA': freqs[alt_allele] += 1-float(freq)
+		total = sum(freqs.values())
+		if total > 0:
+			for allele in freqs:
+				freqs[allele] = freqs[allele]/total
+		return freqs
+		
+	def compute_refalt_freq(self):
+		""" compute combined frequency of reference and major alternate allele """
+		freqs = self.allele_freqs
+		ref_freq = freqs[self.ref_allele]
+		alt_freq = 0
+		for allele, freq in self.allele_freqs.items():
+			if allele != self.ref_allele:
+				alt_freq = max(freq, alt_freq)
+		return ref_freq + alt_freq
 
-	def mean_freq(self, weighted):
-		if weighted:
+	def compute_mean_freq(self, weighted):
+		""" compute average frequency of reference allele with optional weighting of samples """
+		if len(self.ref_freq) == 0:
+			return 0.0
+		elif weighted:
 			sum_depth = sum(site.depth)
 			weighted_freqs = [freq * depth / sum_depth for freq, depth in zip(self.ref_freq, self.depth)]
 			return np.mean(weighted_freqs)
 		else:
 			return np.mean(self.ref_freq)
-		
-	def pooled_pi(self, site_depth, weighted=False):
-		x = self.maf(site_depth, weighted)
-		if x == 'NA': return 0
-		else: return 2*x*(1-x)
 
 	def prune(self, indexes):
-		""" prune low quality samples """
+		""" subset site based on list of indexes """
 		self.ref_freq = [self.ref_freq[i] for i in indexes]
 		self.depth = [self.depth[i] for i in indexes]
 		self.alt_allele = [self.alt_allele[i] for i in indexes]
 		self.samples = [self.samples[i] for i in indexes]
 				
 	def prune_samples(self, site_depth, site_ratio):
-		""" remove data for samples with low coverage at site """
+		""" filter samples at site based on coverage """
 		index = 0
 		indexes = []
 		for depth, sample in zip(self.depth, self.samples):
@@ -72,64 +108,23 @@ class GenomicSite:
 					depth/sample.depth <= site_ratio):
 				indexes.append(index)
 			index += 1
-
 		self.prune(indexes)
 
-	def resample_reads(self, rand_reads):
+	def resample_reads(self, rand_reads, replace_reads):
+		""" resample random number of reads per sample """
 		index = 0
 		for ref_freq, depth in zip(self.ref_freq, self.depth):
-			if depth > rand_reads:
+			if depth > rand_reads or replace_reads:
 				self.depth[index] = rand_reads
 				if ref_freq > 0 and ref_freq < 1:
 					count_ref = int(round(ref_freq * depth))
 					count_alt = depth - count_ref
-					alleles = random.sample([1]*count_ref+[0]*count_alt, rand_reads)
+					alleles = np.random.choice([1]*count_ref+[0]*count_alt, rand_reads, replace=replace_reads)
 					self.ref_freq[index] = np.mean(alleles)
 			index += 1
 
-	def resample_samples(self, rand_samples):
-		indexes = random.sample(range(len(self.samples)), rand_samples)
-		self.ref_freq = [self.ref_freq[i] for i in indexes]
-		self.depth = [self.depth[i] for i in indexes]
-		self.samples = [self.samples[i] for i in indexes]
-		
-	def cast_numeric(self):
-		self.ref_freq = [float(_) if _ != 'NA' else 0.0 for _ in self.ref_freq ]
-		self.depth = [int(_) if _ != 'NA' else 0.0 for _ in self.depth]
-	
-	def parse_id(self):
-		ref_id = self.id.rsplit('|', 2)[0]
-		ref_pos = int(self.id.split('|')[2])
-		ref_allele = self.id.split('|')[3] if len(self.id.split('|')) >= 4 else '' # testing
-		return ref_id, ref_pos, ref_allele
-
-	def sample_values(self): # return a dic mapping sample id to site values
-		d = {}
-		for index, sample in enumerate(self.samples):
-			d[sample] = {}
-			d[sample]['ref_freq'] = self.ref_freq[index]
-			d[sample]['depth'] = self.depth[index]
-			d[sample]['alt_allele'] = self.alt_allele[index]
-		return d
-
-	def allele_props(self):
-		sums = {'A':0.0, 'T':0.0, 'C':0.0, 'G':0.0}
-		props = {}
-		# sum alternate allele proportions
-		for freq, allele in zip(self.ref_freq, self.alt_allele):
-			if allele != 'NA': sums[allele] += 1-float(freq)
-		# sum reference allele proportions
-		for freq in self.ref_freq:
-			if freq != 'NA': sums[self.ref_allele] += float(freq)
-		# normalize proportions
-		total = sum(sums.values())
-		if total > 0:
-			for allele in sums:
-				props[allele] = float('%.3g' % (sums[allele]/total))
-		return props
-
 def parse_tsv(inpath):
-	""" yield records from tab-delimited file with header """
+	""" yield records from tab-delimited file with row and column names """
 	infile = utility.iopen(inpath)
 	header = next(infile).rstrip('\n').split('\t')
 	for line in infile:
@@ -146,7 +141,6 @@ def parse_sites(indir, samples):
 	for ext in ['alt_allele', 'depth', 'ref_freq']:
 		files[ext] = parse_tsv('%s/snps_%s.txt' % (indir, ext))
 	info = utility.parse_file('%s/snps_info.txt' % indir)
-
 	while True: # yield GenomicSite
 		site = GenomicSite(files, samples, info)
 		if not site.id:
@@ -159,22 +153,22 @@ def parse_sites(indir, samples):
 
 class Sample:
 	""" Base class for sample """
-	def __init__(self, info, filters, stop):
+	def __init__(self, info):
 		self.id = info['sample_id']
-		self.depth = float(info['mean_coverage'])
-		self.pass_qc = self.qc(info, filters, stop)
+		self.info = info
+		self.depth = float(self.info['mean_coverage'])
+		self.covered = float(self.info['fraction_covered'])
 
-	def qc(self, info, filters, stop):
-		if stop:
-			return False
-		elif float(info['fraction_covered']) < filters['min_cov']:
-			return False
-		elif float(info['mean_coverage']) < filters['min_depth']:
-			return False
-		else:
+	def filter(self, sample_depth, fract_cov):
+		if self.covered < fract_cov:
 			return True
+		elif self.depth < sample_depth:
+			return True
+		else:
+			return False
 
 def fetch_samples(args):
+	""" initialize samples from indir and set pass_qc flag """
 	samples = []
 	count_hq = 0
 	stop = False
--- scripts/snp_diversity.py
+++ scripts/snp_diversity.py
@@ -5,7 +5,7 @@
 # Freely distributed under the GNU General Public License (GPLv3)
 
 import argparse, sys, os, gzip, numpy as np, random
-from midas import utility
+from midas.utility import print_copyright, parse_file
 from midas.analyze import snp_matrix
 
 def parse_arguments():
@@ -32,7 +32,7 @@ directory should be named according to a species_id and contains files 'snps_*.t
 		help="""path to output file containing list of markers""")
 
 	diversity = parser.add_argument_group("Diversity options")
-	diversity.add_argument('--genomic_type', choices=['genome-wide'], default='genome-wide',
+	diversity.add_argument('--genomic_type', choices=['genome-wide', 'per-gene'], default='genome-wide',
 		help="""compute diversity for individual genes or genome-wide (genome-wide)""")
 	diversity.add_argument('--sample_type', choices=['per-sample', 'pooled-samples'], default='per-sample',
 		help="""compute diversity for individual samples or for pooled reads across samples (per-sample)""")
@@ -43,14 +43,14 @@ ALL=all-sites, NC=non-coding, CDS=coding, XD=X-fold-degenerate-sites""")
 		help="""weight samples by sequencing depth when --sample_type=pooled-samples""")
 	diversity.add_argument('--rand_reads', type=int, metavar='INT',
 		help="""randomly select N reads from each sample for each genomic site """)
+	diversity.add_argument('--replace_reads', action='store_true', default=False,
+		help="""reads drawn with replacement""")
 	diversity.add_argument('--rand_samples', type=int, metavar='INT',
 		help="""randomly select N samples from each genomic site""")
-	diversity.add_argument('--rand_sites', type=int, metavar='INT',
-		help="""randomly select N genomic sites""")
-		
+	diversity.add_argument('--rand_sites', type=float, metavar='FLOAT',
+		help="""randomly select X proportion of high-quality genomic sites""")
+
 	sample = parser.add_argument_group("Sample filters (select subset of samples from INDIR)")
-	sample.add_argument('--samples', type=str, metavar='STR', required=False,
-		help="""comma-separated list of samples to use for computing diversity metrics""")
 	sample.add_argument('--sample_depth', dest='sample_depth', type=float, default=0.0, metavar='FLOAT',
 		help="""minimum average read depth per sample (0.0)""")
 	sample.add_argument('--fract_cov', dest='fract_cov', type=float, default=0.0, metavar='FLOAT',
@@ -58,14 +58,20 @@ ALL=all-sites, NC=non-coding, CDS=coding, XD=X-fold-degenerate-sites""")
 	sample.add_argument('--max_samples', type=int, metavar='INT', default=float('Inf'),
 		help="""maximum number of samples to process.
 useful for quick tests (use all)""")
+	sample.add_argument('--keep_samples', type=str, metavar='STR', required=False,
+		help="""comma-separated list of samples to use for computing diversity metrics.
+samples will still be subject to other filters""")
+	sample.add_argument('--exclude_samples', type=str, metavar='STR', required=False,
+		help="""comma-separated list of samples to exclude from computing diversity metrics.
+samples will still be subject to other filters""")
 
 	snps = parser.add_argument_group("Site filters (select subset of genomic sites from INDIR)")
-	snps.add_argument('--site_depth', type=int, default=3, metavar='INT',
-		help="""minimum number of mapped reads per site (3)
+	snps.add_argument('--site_depth', type=int, default=2, metavar='INT',
+		help="""minimum number of mapped reads per site (2)
 a high value like 20 will result in accurate allele frequencies, but may discard many sites.
-a low value like 1 will retain many sites but may not result in accurate allele frequencies""")
-	snps.add_argument('--site_prev', type=float, default=0.95, metavar='FLOAT',
-		help="""site has at least <site_depth> coverage in at least <site_prev> proportion of samples (0.95)
+a low value like 2 will retain many sites but may not result in accurate allele frequencies""")
+	snps.add_argument('--site_prev', type=float, default=0.0, metavar='FLOAT',
+		help="""site has at least <site_depth> coverage in at least <site_prev> proportion of samples (0.0)
 a value of 1.0 will select sites that have sufficent coverage in all samples.
 a value of 0.0 will select all sites, including those with low coverage in many samples 
 NAs recorded for included sites with less than <site_depth> in a sample """)
@@ -76,17 +82,32 @@ by default invariant sites are also retained.""")
 	snps.add_argument('--site_ratio', type=float, default=float('Inf'), metavar='INT',
 		help="""maximum ratio of site-depth to mean-genome-depth (None)
 a value of 10 will filter genomic sites with 10x high coverage than the genomic background""")
+	snps.add_argument('--site_freq', type=float, default=0.0, metavar='FLOAT',
+		help="""minimum combined frequency of reference allele and major alternate allele across samples (0.0)
+most mapped reads should match the reference allele or major alternate allele
+set this value to exclude sites with multiple alternate alleles
+for example, --site_freq=0.95 excludes these sites:
+  A(ref)=0.80, T(alt1)=0.10, C(alt2)=0.05, G(alt3)=0.05 (Freq_A + Freq_T = 0.90 < 0.95)
+  A(ref)=0.00, C(alt1)=0.80, G(alt2)=0.20, T(alt3)=0.00 (Freq_A + Freq_C = 0.80 < 0.95)""")
 	snps.add_argument('--max_sites', type=int, default=float('Inf'), metavar='INT',
 		help="""maximum number of sites to include in output (use all)
 useful for quick tests""")
 	args = vars(parser.parse_args())
-	if args['site_type'] == 'ALL': # format site_type
+	format_site_type(args)
+	format_sample_lists(args)
+	return args
+
+def format_sample_lists(args):
+	keep = args['keep_samples'].rstrip(',').split(',') if args['keep_samples'] else None
+	exclude = args['exclude_samples'].rstrip(',').split(',') if args['exclude_samples'] else None
+
+def format_site_type(args):
+	if args['site_type'] == 'ALL':
 		args['site_type'] = ['NC','1D','2D','3D','4D']
 	elif args['site_type'] == 'CDS':
 		args['site_type'] = ['1D','2D','3D','4D']
 	else:
 		args['site_type'] = [args['site_type']]
-	return args
 
 def print_args(args):
 	lines = []
@@ -100,18 +121,22 @@ def print_args(args):
 	lines.append("   -site_type: %s" % args['site_type'])
 	lines.append("   -weight_by_depth: %s" % args['weight_by_depth'])
 	lines.append("   -rand_reads: %s" % args['rand_reads'])
+	lines.append("   -replace_reads: %s" % args['replace_reads'])
+	lines.append("   -rand_samples: %s" % args['rand_samples'])
 	lines.append("   -rand_sites: %s" % args['rand_sites'])
 	lines.append("Sample filters:")
-	lines.append("   --samples: %s" % args['samples'])
-	lines.append("   --sample_depth: %s" % args['sample_depth'])
-	lines.append("   --fract_cov: %s" % args['fract_cov'])
-	lines.append("   --max_samples: %s" % args['max_samples'])
+	lines.append("   -sample_depth: %s" % args['sample_depth'])
+	lines.append("   -fract_cov: %s" % args['fract_cov'])
+	lines.append("   -max_samples: %s" % args['max_samples'])
+	lines.append("   -keep_samples: %s" % args['keep_samples'])
+	lines.append("   -exclude_samples: %s" % args['exclude_samples'])
 	lines.append("Site filters:")
-	lines.append("   --site_depth: %s" % args['site_depth'])
-	lines.append("   --site_prev: %s" % args['site_prev'])
-	lines.append("   --site_maf: %s" % args['site_maf'])
-	lines.append("   --site_ratio: %s" % args['site_ratio'])
-	lines.append("   --max_sites: %s" % args['site_ratio'])
+	lines.append("   -site_depth: %s" % args['site_depth'])
+	lines.append("   -site_prev: %s" % args['site_prev'])
+	lines.append("   -site_maf: %s" % args['site_maf'])
+	lines.append("   -site_ratio: %s" % args['site_ratio'])
+	lines.append("   -site_freq: %s" % args['site_freq'])
+	lines.append("   -max_sites: %s" % args['max_sites'])
 	sys.stdout.write('\n'.join(lines)+'\n')
 
 def check_args(args):
@@ -135,8 +160,12 @@ def check_args(args):
 		sys.exit("\nError: --site_prev must be between 0 and 1")
 	if not 0 <= args['fract_cov'] <= 1:
 		sys.exit("\nError: --fract_cov must be between 0 and 1")
-	if args['rand_reads'] > args['site_depth']:
-		sys.exit("\nError: --rand_reads cannot exceed --site_depth")
+	if args['rand_reads'] > args['site_depth'] and not args['replace_reads']:
+		sys.exit("\nError: --rand_reads cannot exceed --site_depth when --replace_reads=False")
+	if args['rand_sites'] and (args['rand_sites'] < 0 or args['rand_sites'] > 1):
+		sys.exit("\nError: --rand_sites must be between 0 and 1")
+	if 'NC' in args['site_type'] and args['genomic_type'] == 'per-gene':
+		sys.exit("\nError: --site_type cannot be NC if --genomic_type is per-gene")
 
 class Diversity:
 	def __init__(self):
@@ -144,9 +173,19 @@ class Diversity:
 		self.samples = 0
 		self.snps = 0
 		self.pi = 0
+		self.depth = 0
+
+def list_genes(args):
+	""" List the set of genes from snps_info.txt """
+	genes = set([])
+	for r in parse_file('%s/snps_info.txt' % args['indir']):
+		if r['gene_id'] != '': genes.add(r['gene_id'])
+	return genes
 
 def init_pi(args, samples):
+	""" Initialize dictionary to store nucleotide diversity statistics """
 	pi = {}
+	count_samples = len([_ for _ in samples if _.pass_qc])
 	if args['sample_type'] == 'per-sample':
 		for s in samples:
 			if s.pass_qc:
@@ -154,14 +193,16 @@ def init_pi(args, samples):
 					pi[s.id] = Diversity()
 				else:
 					pi[s.id] = {}
-					for g in genes:
-						pi[s.id][g.id] = Diversity()
+					for gene in list_genes(args):
+						pi[s.id][gene] = Diversity()
 	else:
 		if args['genomic_type'] == 'genome-wide':
 			pi = Diversity()
+			pi.samples = count_samples
 		else:
-			for g in genes:
-				pi[s.id][g.id] = Diversity()
+			for gene in list_genes(args):
+				pi[gene] = Diversity()
+				pi[gene].samples = count_samples
 	return pi
 
 def maf(x):
@@ -173,75 +214,124 @@ def compute_pi(args, samples):
 	for site in snp_matrix.parse_sites(args['indir'], samples): # loop over sites
 		
 		# stop early
-		if index >= args['max_sites']:
-			break
+		if index >= args['max_sites']: break
 			
 		# prune low quality samples
 		site.prune_samples(args['site_depth'], args['site_ratio'])
-		
+
 		# compute site summary stats
 		site.summary_stats(args['weight_by_depth'])
 		
-		# filter site
-		if site.filter_site(args['site_prev'], args['site_maf'], args['site_type']):
-			continue
-		else:
-			index += 1
-
-		# downsample samples
-		if args['rand_samples']:
-			site.resample_samples(args['rand_samples'])
-		
+		#  filter by prevalence, frequency, site type, # of alt alleles
+		filter, code = site.filter_site(args['site_prev'], args['site_maf'], args['site_type'], args['site_freq'])
+		if filter: continue
+		#  filter 1 - args['rand_sites'] proportion of sites
+		elif args['rand_sites'] and random.uniform(0, 1) > args['rand_sites']: continue
+		#  keep site
+		else: index += 1
+			
 		# downsample reads
 		if args['rand_reads'] and site.maf > 0:
-			site.resample_reads(args['rand_reads'])
-		
+			site.resample_reads(args['rand_reads'], args['replace_reads'])
+
 		# compute pi for pooled-samples
 		if args['sample_type'] == 'pooled-samples':
-			site_pi = site.pooled_pi(args['site_depth'], args['weight_by_depth'])
-			pi.pi += site_pi
-			pi.sites += 1
+			site_pi = 2 * site.maf * (1-site.maf)
+			if args['genomic_type'] == 'genome-wide':
+				pi.pi += site_pi
+				pi.sites += 1
+			else:
+				pi[site.info['gene_id']].pi += site_pi
+				pi[site.info['gene_id']].sites += 1
 
-		# compute pi for per-samples
+		# compute pi per-sample
 		else:
-			for sample, values in site.sample_values().items():
-				if values['depth'] < args['site_depth']:
-					continue
-				if args['rand_reads'] and maf(values['ref_freq']) > 0: # downsample reads
-					count_ref = int(round(values['ref_freq'] * values['depth']))
-					count_alt = values['depth'] - count_ref
-					alleles = random.sample([1] * count_ref + [0] * count_alt, args['rand_reads'])
-					values['ref_freq'] = np.mean(alleles)
-				site_maf = maf(values['ref_freq'])
-				pi[sample.id].pi += 2 * site_maf * (1-site_maf)
-				pi[sample.id].sites += 1
+			for sample, ref_freq, depth in zip(site.samples, site.ref_freq, site.depth):
+				site_maf = maf(ref_freq)
+				if args['genomic_type'] == 'genome-wide':
+					pi[sample.id].pi += 2 * site_maf * (1-site_maf)
+					pi[sample.id].sites += 1
+					pi[sample.id].depth += depth
+				else:
+					pi[sample.id][site.info['gene_id']].pi += 2 * site_maf * (1-site_maf)
+					pi[sample.id][site.info['gene_id']].sites += 1
+					pi[sample.id][site.info['gene_id']].depth += depth
 	return pi
 
 def write_pi(args, samples, pi):
+	""" Write nucleotide diversity results to specified output file """
 	outfile = open(args['out'], 'w')
 	if args['sample_type'] == 'pooled-samples':
-		h = ['count_sites', 'diversity']
-		outfile.write('\t'.join([str(_) for _ in h])+'\n')
-		r = [pi.sites, pi.pi]
-		outfile.write('\t'.join([str(_) for _ in r])+'\n')
-	else:
-		h = ['sample_id', 'count_sites', 'diversity']
+		if args['genomic_type'] == 'genome-wide':
+			h = ['count_samples', 'count_sites', 'diversity']
+			outfile.write('\t'.join([str(_) for _ in h])+'\n')
+			r = [pi.samples, pi.sites, pi.pi]
+			outfile.write('\t'.join([str(_) for _ in r])+'\n')
+		else:
+			h = ['gene_id', 'count_samples', 'count_sites', 'diversity']
+			outfile.write('\t'.join([str(_) for _ in h])+'\n')
+			for gene in pi:
+				r = [gene, pi[gene].samples, pi[gene].sites, pi[gene].pi]
+				outfile.write('\t'.join([str(_) for _ in r])+'\n')
+	elif args['genomic_type'] == 'genome-wide':
+		h = ['sample_id', 'depth', 'count_sites', 'diversity']
 		outfile.write('\t'.join([str(_) for _ in h])+'\n')
 		for s in samples:
-			if s.pass_qc:
-				r = [s.id, pi[s.id].sites, pi[s.id].pi]
+			if not s.pass_qc: continue
+			r = [s.id, pi[s.id].depth, pi[s.id].sites,  pi[s.id].pi]
+			outfile.write('\t'.join([str(_) for _ in r])+'\n')
+	else:
+		h = ['sample_id', 'gene_id', 'depth', 'count_sites', 'diversity']
+		outfile.write('\t'.join([str(_) for _ in h])+'\n')
+		for s in samples:
+			if not s.pass_qc: continue
+			for gene in pi[s.id]:
+				r = [s.id, gene, pi[s.id][gene].depth, pi[s.id][gene].sites,  pi[s.id][gene].pi]
 				outfile.write('\t'.join([str(_) for _ in r])+'\n')
+	outfile.close()
+
+def resample_samples(samples):
+	""" Random select high quality samples, set pass_qc=False for others """
+	hq_indexes = [index for index, sample in enumerate(samples) if sample.pass_qc]
+	if args['rand_samples'] > len(hq_indexes):
+		sys.exit("\nError: --rand_samples cannot exceed the number of high-quality samples")
+	else:
+		hq_indexes = np.random.choice(hq_indexes, args['rand_samples'], replace=False)
+		for index, sample in enumerate(samples):
+			if index not in hq_indexes: sample.pass_qc = False
+
+def fetch_samples(args):
+	""" List and select samples from input """
+	from midas.analyze.snp_matrix import Sample
+	samples = []
+	for info in parse_file('%s/snps_summary.txt' % args['indir']):
+		# init sample
+		sample = Sample(info)
+		sample.pass_qc = True
+		# keep/exclude sample
+		if sample.filter(args['sample_depth'], args['fract_cov']):
+			sample.pass_qc = False
+		if args['keep_samples'] and sample.id not in args['keep_samples']:
+			sample.pass_qc = False
+		if args['exclude_samples'] and sample.id in args['exclude_samples']:
+			sample.pass_qc = False
+		if sum([1 for s in samples if s.pass_qc]) >= args['max_samples']:
+			sample.pass_qc = False
+		# store sample
+		samples.append(sample)
+	# select random sample
+	if args['rand_samples']: resample_samples(samples)
+	return samples
 
 if __name__ == '__main__':
 	args = parse_arguments()
 	check_args(args)
-	utility.print_copyright()
+	print_copyright()
 	print_args(args)
 	
-	print("Selecting subset of samples...")
-	samples = snp_matrix.fetch_samples(args)
-	print("   %s samples selected") % len([_ for _ in samples if _.pass_qc])
-	
+	print("\nSelecting subset of samples...")
+	samples = fetch_samples(args)
+
 	print("Estimating diversity metrics...")
 	pi = compute_pi(args, samples)
 
