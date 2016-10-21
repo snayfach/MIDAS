c2ef52cd3c0a91ef9469743f6c8d8bee29fd0778
snayfach <snayfach@gmail.com>
2016-10-21 14:28:08 -0700

    updated strain tracking script -now uses midas.parse.py -now takes list of sample_ids for training -no longer takes sample mapping file -changed --max_groups to --allele_freq

--- scripts/strain_tracking.py
+++ scripts/strain_tracking.py
@@ -4,8 +4,7 @@
 # Copyright (C) 2015 Stephen Nayfach
 # Freely distributed under the GNU General Public License (GPLv3)
 
-import argparse, sys, os, platform, itertools
-from operator import itemgetter
+import argparse, sys, os
 from midas import utility
 
 def get_program():
@@ -48,20 +47,20 @@ Usage: strain_tracking.py id_markers [options]
 """,
 	epilog="""
 Examples:
-1) Identify marker alleles for species_id 57955 which are found in exactly one sample
-strain_tracking.py id_markers --indir merged_snps/57955 --out 57955.markers --max_groups 1
+1) Identify marker alleles for one species
+strain_tracking.py id_markers --indir merged_snps/species_id --out species.markers --max_samples 1
 
-2) Run a quick test for species_id 57955
-strain_tracking.py id_markers --indir merged_snps/57955 --out 57955.markers --max_sites 10000
+2) Run a quick test
+strain_tracking.py id_markers --indir merged_snps/species_id --out species.markers --max_sites 10000
 
 Output fields:
-  site_id: site identifier
-  allele: nucleotide (A, T, C, or G)
-  count_samples - number of sample-groups with non-zero coverage at site_id
-  count_A: number of sample-groups with A at site_id
-  count_T: number of sample-groups with T at site_id
-  count_C: number of sample-groups with C at site_id
-  count_G: number of sample-groups with G at site_id
+  site_id: site identifier (format: ref_id|ref_pos|ref_allele)
+  allele: nucleotide
+  count_samples - # of samples with sufficient coverage at site_id
+  count_A: # of samples with A at site_id
+  count_T: # of samples with T at site_id
+  count_C: # of samples with C at site_id
+  count_G: # of samples with G at site_id
 """)
 	parser.add_argument('program', help=argparse.SUPPRESS)
 	parser.add_argument('--indir', metavar='PATH', type=str, required=True,
@@ -69,30 +68,20 @@ Output fields:
 requires having run 'merge_midas.py snps'""")
 	parser.add_argument('--out', metavar='PATH', type=str, required=True,
 		help="""path to output file containing list of markers""")
-	parser.add_argument('--sample_map', metavar='PATH', type=str,
-		help="""path to mapping file that specifes groups of related sample_ids.
-the file should be tab-delimited with no header and two columns
-example:
-   sample1 subject1
-   sample2 subject1
-   sample3 subject2
-   sample4 subject2
-   sample5 subject3
-""")
+	parser.add_argument('--samples', metavar='PATH', type=str,
+		help="""comma-separated list of training samples\nby default, all samples are used""")
 	parser.add_argument('--min_freq', type=float, metavar='FLOAT', default=0.10,
 		help="""minimum allele frequency (proportion of reads) per site for SNP calling (0.10)""")
 	parser.add_argument('--min_reads', type=int, metavar='INT', default=3,
 		help="""minimum number of reads supporting allele per site for SNP calling (3)""")
-	parser.add_argument('--max_groups', type=int, metavar='INT', default=1,
-		help="""discard alleles found in > MAX_GROUPS (1)
-groups are specified by the second field in '--sample_map'
-if '--sample_map' is not specified, each sample_id is treated as a separate group""")
-	parser.add_argument('--max_sites', type=int, metavar='INT',
+	parser.add_argument('--allele_prev', type=int, metavar='INT', default=1,
+		help="""maximum occurences of allele across samples (1)
+setting this to 1 (default) will pick alleles found in exactly 1 sample""")
+	parser.add_argument('--max_sites', type=int, metavar='INT', default=float('inf'),
 		help="""maximum number of genomic sites to process (use all)
 useful for quick tests""")
 	args = vars(parser.parse_args())
-	if args['sample_map']: args['samples'] = dict([_.rstrip().split() for _ in open(args['sample_map'])])
-	else: args['samples'] = dict([(_.split()[0],_.split()[0]) for _ in open('%s/snps_summary.txt' % args['indir'])])
+	if args['samples']: args['samples'] = args['samples'].split(',')
 	return args
 
 def track_arguments():
@@ -133,7 +122,7 @@ requires having run 'merge_midas.py snps'""")
 		help="""minimum allele frequency (proportion of reads) per site for SNP calling (0.10)""")
 	parser.add_argument('--min_reads', type=int, metavar='INT', default=3,
 		help="""minimum number of reads supporting allele per site for SNP calling (3)""")
-	parser.add_argument('--max_sites', type=int, metavar='INT',
+	parser.add_argument('--max_sites', type=int, metavar='INT', default=float('inf'),
 		help="""maximum number of sites to process (use all)
 useful for quick tests""")
 	parser.add_argument('--max_samples', type=int, metavar='INT',
