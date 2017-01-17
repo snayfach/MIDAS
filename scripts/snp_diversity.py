946a6d89a19d456416762b518ba1cbc4308fa8fe
Stephen Nayfach <snayfach@shattuck.ucsf.edu>
2017-01-16 18:58:14 -0800

    Not-tested update to snp_diversity.py

--- scripts/snp_diversity.py
+++ scripts/snp_diversity.py
@@ -4,9 +4,9 @@
 # Copyright (C) 2015 Stephen Nayfach
 # Freely distributed under the GNU General Public License (GPLv3)
 
-import argparse, sys, os, gzip, numpy as np, random
-from midas.utility import print_copyright, parse_file
-from midas.analyze import diversity
+import argparse, sys, os, numpy as np, random, csv
+from midas.utility import print_copyright
+from midas.analyze import parse_snps
 
 def parse_arguments():
 	""" Parse command line arguments """
@@ -49,6 +49,8 @@ ALL=all-sites, NC=non-coding, CDS=coding, XD=X-fold-degenerate-sites""")
 		help="""randomly select N samples from each genomic site""")
 	diversity.add_argument('--rand_sites', type=float, metavar='FLOAT',
 		help="""randomly select X proportion of high-quality genomic sites""")
+	diversity.add_argument('--snp_maf', type=float, metavar='FLOAT', default=0.01,
+		help="""minor allele frequency cutoff for determining if a site is a SNP (0.01)""")
 
 	sample = parser.add_argument_group("Sample filters (select subset of samples from INDIR)")
 	sample.add_argument('--sample_depth', dest='sample_depth', type=float, default=0.0, metavar='FLOAT',
@@ -66,6 +68,8 @@ samples will still be subject to other filters""")
 samples will still be subject to other filters""")
 
 	snps = parser.add_argument_group("Site filters (select subset of genomic sites from INDIR)")
+	snps.add_argument('--site_list', metavar='PATH',
+		help="""path to file containing newline-delimited list of genomic sites to include.\nother filters will still apply""")
 	snps.add_argument('--site_depth', type=int, default=2, metavar='INT',
 		help="""minimum number of mapped reads per site (2)""")
 	snps.add_argument('--site_prev', type=float, default=0.0, metavar='FLOAT',
@@ -179,86 +183,125 @@ class Diversity:
 def list_genes(args):
 	""" List the set of genes from snps_info.txt """
 	genes = set([])
-	for r in parse_file('%s/snps_info.txt' % args['indir']):
+	path = '%s/snps_info.txt' % args['indir']
+	for r in csv.DictReader(open(path), delimiter='\t'):
 		if r['gene_id'] != '': genes.add(r['gene_id'])
 	return genes
 
 def init_pi(args, samples):
 	""" Initialize dictionary to store nucleotide diversity statistics """
 	pi = {}
-	count_samples = len([_ for _ in samples if _.pass_qc])
 	if args['sample_type'] == 'per-sample':
-		for s in samples:
-			if s.pass_qc:
-				if args['genomic_type'] == 'genome-wide':
-					pi[s.id] = Diversity()
-				else:
-					pi[s.id] = {}
-					for gene in list_genes(args):
-						pi[s.id][gene] = Diversity()
-	else:
+		if args['genomic_type'] == 'genome-wide':
+			for s in samples.values(): pi[s.id] = Diversity()
+		elif args['genomic_type'] == 'per-gene':
+			genes = list_genes(args)
+			for s in samples.values():
+				pi[s.id] = {}
+				for gene in genes: pi[s.id][gene] = Diversity()
+	elif args['sample_type'] == 'pooled-samples':
 		if args['genomic_type'] == 'genome-wide':
 			pi = Diversity()
-			pi.samples = count_samples
-		else:
-			for gene in list_genes(args):
+			pi.samples = len(samples)
+		elif args['genomic_type'] == 'per-gene':
+			genes = list_genes(args)
+			for gene in genes:
 				pi[gene] = Diversity()
-				pi[gene].samples = count_samples
+				pi[gene].samples = len(samples)
 	return pi
 
-def maf(x):
-	return x if x <= 0.5 else 1-x
+def compute_maf(freq):
+	""" Compute minor allele frequency """
+	return min(freq, 1-freq)
 
-def compute_pi(args, samples):
+def compute_pi(freq):
+	""" Compute diversity based on minor allele frequency """
+	return 2*freq*(1-freq)
+
+def is_snp(freq, min_maf):
+	""" Determine if a genomic site is a SNP or not """
+	maf = compute_maf(freq)
+	if maf >= min_maf:
+		return True
+	else:
+		return False
+
+def compute_snp_diversity(args, species, samples):
+
 	pi = init_pi(args, samples)
+	
+	# read list of genomic sites to keep
+	if args['site_list']:
+		site_list = [_.rstrip() for _ in open(args['site_list'])]
+		site_index = 0
+	
 	index = 0
-	for site in diversity.parse_sites(args['indir'], samples): # loop over sites
-		
+	for site in parse_snps.fetch_sites(species, samples):
+
 		# stop early
 		if index >= args['max_sites']: break
+		
+		# skip sites not in site_list
+		if args['site_list']:
+			if site_index >= len(site_list):
+				break
+			elif site.id != site_list[site_index]:
+				continue
+			else:
+				site_index += 1
 			
-		# prune low quality samples for site
-		site.prune_samples(args['site_depth'], args['site_ratio'])
-
+		#  skip random subset of genomic sites
+		if args['rand_sites'] and random.uniform(0, 1) > args['rand_sites']:
+			continue
+			
+		# prune low quality samples for site:
+		#   site.samples['sample'].keep = [True|False]
+		site.flag_samples(args['site_depth'], args['site_ratio'])
+		
 		# compute site summary stats
+		#   site.prevalence
+		#   site.pooled_maf
 		site.summary_stats(args['weight_by_depth'])
 		
-		#  filter by prevalence, frequency, site type, # of alt alleles
-		filter, code = site.filter_site(args['site_prev'], args['site_maf'], args['site_type'], args['site_freq'])
-		if filter: continue
-		#  filter 1 - args['rand_sites'] proportion of sites
-		elif args['rand_sites'] and random.uniform(0, 1) > args['rand_sites']: continue
-		#  keep site
-		else: index += 1
-			
-		# downsample reads
-		if args['rand_reads'] and site.maf > 0:
+		# filter genomic site
+		#   site.keep = [True|False]
+		site.filter(args['site_prev'], args['site_maf'], args['site_type'])
+		if not site.keep:
+			continue
+		else:
+			index += 1
+
+		# downsample reads & recompute pooled frequency
+		if args['rand_reads'] and site.pooled_maf > 0.0:
 			site.resample_reads(args['rand_reads'], args['replace_reads'])
-			site.mean_freq = site.compute_mean_freq(args['weight_by_depth'])
-			site.maf = min(site.mean_freq, 1-site.mean_freq)
+			site.pooled_maf = site.compute_pooled_maf(args['weight_by_depth'])
 
 		# compute pi for pooled-samples
 		if args['sample_type'] == 'pooled-samples':
-			site_pi = 2 * site.maf * (1-site.maf)
 			if args['genomic_type'] == 'genome-wide':
-				pi.pi += site_pi
+				pi.pi += compute_pi(site.pooled_maf)
+				pi.snps += 1 if is_snp(site.pooled_maf, args['snp_maf']) else 0
 				pi.sites += 1
 			else:
-				pi[site.info['gene_id']].pi += site_pi
-				pi[site.info['gene_id']].sites += 1
+				pi[site.gene_id].pi += compute_pi(site.pooled_maf)
+				pi[site.gene_id].snps += 1 if is_snp(site.pooled_maf, args['snp_maf']) else 0
+				pi[site.gene_id].sites += 1
 
 		# compute pi per-sample
 		else:
-			for sample, ref_freq, depth in zip(site.samples, site.ref_freq, site.depth):
-				site_maf = maf(ref_freq)
-				if args['genomic_type'] == 'genome-wide':
-					pi[sample.id].pi += 2 * site_maf * (1-site_maf)
-					pi[sample.id].sites += 1
-					pi[sample.id].depth += depth
-				else:
-					pi[sample.id][site.info['gene_id']].pi += 2 * site_maf * (1-site_maf)
-					pi[sample.id][site.info['gene_id']].sites += 1
-					pi[sample.id][site.info['gene_id']].depth += depth
+			for sample in site.samples.values():
+				if sample.keep:
+					if args['genomic_type'] == 'genome-wide':
+						pi[sample.id].pi += compute_pi(sample.freq)
+						pi[sample.id].snps += 1 if is_snp(sample.freq, args['snp_maf']) else 0
+						pi[sample.id].sites += 1
+						pi[sample.id].depth += sample.depth
+					else:
+						pi[sample.id][site.gene_id].pi += compute_pi(sample.freq)
+						pi[sample.id][site.gene_id].snps += 1 if is_snp(sample.freq, args['snp_maf']) else 0
+						pi[sample.id][site.gene_id].sites += 1
+						pi[sample.id][site.gene_id].depth += sample.depth
+								
 	return pi
 
 def write_pi(args, samples, pi):
@@ -266,77 +309,46 @@ def write_pi(args, samples, pi):
 	outfile = open(args['out'], 'w')
 	if args['sample_type'] == 'pooled-samples':
 		if args['genomic_type'] == 'genome-wide':
-			h = ['count_samples', 'count_sites', 'diversity']
+			h = ['count_samples', 'count_sites', 'count_snps', 'count_pi']
 			outfile.write('\t'.join([str(_) for _ in h])+'\n')
-			r = [pi.samples, pi.sites, pi.pi]
+			r = [pi.samples, pi.sites, pi.snps, pi.pi]
 			outfile.write('\t'.join([str(_) for _ in r])+'\n')
 		else:
-			h = ['gene_id', 'count_samples', 'count_sites', 'diversity']
+			h = ['gene_id', 'count_samples', 'count_sites', 'count_snps', 'count_pi']
 			outfile.write('\t'.join([str(_) for _ in h])+'\n')
 			for gene in pi:
-				r = [gene, pi[gene].samples, pi[gene].sites, pi[gene].pi]
+				r = [gene, pi[gene].samples, pi[gene].sites, pi[gene].snps, pi[gene].pi]
 				outfile.write('\t'.join([str(_) for _ in r])+'\n')
 	elif args['genomic_type'] == 'genome-wide':
-		h = ['sample_id', 'depth', 'count_sites', 'diversity']
+		h = ['sample_id', 'depth', 'count_sites', 'count_snps', 'count_pi']
 		outfile.write('\t'.join([str(_) for _ in h])+'\n')
-		for s in samples:
-			if not s.pass_qc: continue
-			r = [s.id, pi[s.id].depth, pi[s.id].sites,  pi[s.id].pi]
+		for s in samples.values():
+			r = [s.id, pi[s.id].depth, pi[s.id].sites,  pi[s.id].snps, pi[s.id].pi]
 			outfile.write('\t'.join([str(_) for _ in r])+'\n')
 	else:
-		h = ['sample_id', 'gene_id', 'depth', 'count_sites', 'diversity']
+		h = ['sample_id', 'gene_id', 'depth', 'count_sites', 'count_snps', 'count_pi']
 		outfile.write('\t'.join([str(_) for _ in h])+'\n')
-		for s in samples:
-			if not s.pass_qc: continue
+		for s in samples.values():
 			for gene in pi[s.id]:
-				r = [s.id, gene, pi[s.id][gene].depth, pi[s.id][gene].sites,  pi[s.id][gene].pi]
+				r = [s.id, gene, pi[s.id][gene].depth, pi[s.id][gene].sites,  pi[s.id][gene].snps, pi[s.id][gene].pi]
 				outfile.write('\t'.join([str(_) for _ in r])+'\n')
 	outfile.close()
-
-def resample_samples(samples):
-	""" Random select high quality samples, set pass_qc=False for others """
-	hq_indexes = [index for index, sample in enumerate(samples) if sample.pass_qc]
-	if args['rand_samples'] > len(hq_indexes):
-		sys.exit("\nError: --rand_samples cannot exceed the number of high-quality samples\n")
-	else:
-		hq_indexes = np.random.choice(hq_indexes, args['rand_samples'], replace=False)
-		for index, sample in enumerate(samples):
-			if index not in hq_indexes: sample.pass_qc = False
-
-def fetch_samples(args):
-	""" List and select samples from input """
-	from midas.analyze.diversity import Sample
-	samples = []
-	for info in parse_file('%s/snps_summary.txt' % args['indir']):
-		# init sample
-		sample = Sample(info)
-		sample.pass_qc = True
-		# keep/exclude sample
-		if sample.filter(args['sample_depth'], args['fract_cov']):
-			sample.pass_qc = False
-		if args['keep_samples'] and sample.id not in args['keep_samples']:
-			sample.pass_qc = False
-		if args['exclude_samples'] and sample.id in args['exclude_samples']:
-			sample.pass_qc = False
-		if sum([1 for s in samples if s.pass_qc]) >= args['max_samples']:
-			sample.pass_qc = False
-		# store sample
-		samples.append(sample)
-	# select random sample
-	if args['rand_samples']: resample_samples(samples)
-	return samples
 
 if __name__ == '__main__':
 	args = parse_arguments()
 	check_args(args)
 	print_copyright()
 	print_args(args)
-	
+
 	print("\nSelecting subset of samples...")
-	samples = fetch_samples(args)
+	species = parse_snps.Species(args['indir'])
+	samples = parse_snps.fetch_samples(species, args['sample_depth'], args['fract_cov'], args['max_samples'],
+						    		   args['keep_samples'], args['exclude_samples'], args['rand_samples'])
+	print(" %s samples selected" % len(samples))
 
 	print("Estimating diversity metrics...")
-	pi = compute_pi(args, samples)
+	# print % complete ?
+	pi = compute_snp_diversity(args, species, samples)
 
 	print("Writing results to output file...")
 	write_pi(args, samples, pi)
